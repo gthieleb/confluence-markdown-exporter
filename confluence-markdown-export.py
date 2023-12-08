bdd23993af27091479a97a3ab7ea193fac2e93f6
Gunnar Thielebein <gunnar@thielebein.net>
2023-12-08 22:17:09 +0100

    format pep8 with black

--- confluence-markdown-export.py
+++ confluence-markdown-export.py
@@ -9,7 +9,9 @@ from atlassian import Confluence
 
 
 ATTACHMENT_FOLDER_NAME = "attachments"
-DOWNLOAD_CHUNK_SIZE = 4 * 1024 * 1024   # 4MB, since we're single threaded this is safe to raise much higher
+DOWNLOAD_CHUNK_SIZE = (
+    4 * 1024 * 1024
+)  # 4MB, since we're single threaded this is safe to raise much higher
 
 
 class ExportException(Exception):
@@ -22,9 +24,11 @@ class Exporter:
         self.__parsed_url = urlparse(url)
         self.__username = username
         self.__token = token
-        self.__confluence = Confluence(url=urlunparse(self.__parsed_url),
-                                       username=self.__username,
-                                       password=self.__token)
+        self.__confluence = Confluence(
+            url=urlunparse(self.__parsed_url),
+            username=self.__username,
+            password=self.__token,
+        )
         self.__seen = set()
         self.__no_attach = no_attach
         self.__space = space
@@ -33,9 +37,11 @@ class Exporter:
         document_name = document_name_raw
         for invalid in ["..", "/"]:
             if invalid in document_name:
-                print("Dangerous page title: \"{}\", \"{}\" found, replacing it with \"_\"".format(
-                    document_name,
-                    invalid))
+                print(
+                    'Dangerous page title: "{}", "{}" found, replacing it with "_"'.format(
+                        document_name, invalid
+                    )
+                )
                 document_name = document_name.replace(invalid, "_")
         return document_name
 
@@ -47,10 +53,10 @@ class Exporter:
         page = self.__confluence.get_page_by_id(src_id, expand="body.storage")
         page_title = page["title"]
         page_id = page["id"]
-    
+
         # see if there are any children
         child_ids = self.__confluence.get_child_id_list(page_id)
-    
+
         content = page["body"]["storage"]["value"]
 
         # save all files as .html for now, we will convert them later
@@ -75,24 +81,36 @@ class Exporter:
 
         # fetch attachments unless disabled
         if not self.__no_attach:
-            ret = self.__confluence.get_attachments_from_content(page_id, start=0, limit=500, expand=None,
-                                                                 filename=None, media_type=None)
+            ret = self.__confluence.get_attachments_from_content(
+                page_id, start=0, limit=500, expand=None, filename=None, media_type=None
+            )
             for i in ret["results"]:
                 att_title = i["title"]
                 download = i["_links"]["download"]
 
                 att_url = urlunparse(
-                    (self.__parsed_url[0], self.__parsed_url[1], "/wiki/" + download.lstrip("/"), None, None, None)
+                    (
+                        self.__parsed_url[0],
+                        self.__parsed_url[1],
+                        "/wiki/" + download.lstrip("/"),
+                        None,
+                        None,
+                        None,
+                    )
                 )
                 att_sanitized_name = self.__sanitize_filename(att_title)
-                att_filename = os.path.join(page_output_dir, ATTACHMENT_FOLDER_NAME, att_sanitized_name)
+                att_filename = os.path.join(
+                    page_output_dir, ATTACHMENT_FOLDER_NAME, att_sanitized_name
+                )
 
                 att_dirname = os.path.dirname(att_filename)
                 os.makedirs(att_dirname, exist_ok=True)
 
                 print("Saving attachment {} to {}".format(att_title, page_location))
 
-                r = requests.get(att_url, auth=(self.__username, self.__token), stream=True)
+                r = requests.get(
+                    att_url, auth=(self.__username, self.__token), stream=True
+                )
                 if 400 <= r.status_code:
                     if r.status_code == 404:
                         print("Attachment {} not found (404)!".format(att_url))
@@ -106,7 +124,7 @@ class Exporter:
                         f.write(buf)
 
         self.__seen.add(page_id)
-    
+
         # recurse to process child nodes
         for child_id in child_ids:
             self.__dump_page(child_id, parents=sanitized_parents + [page_title])
@@ -123,10 +141,11 @@ class Exporter:
             homepage_id = space["homepage"]["id"]
             self.__dump_page(homepage_id, parents=[space_key])
 
-    
     def dump(self):
-        ret = self.__confluence.get_all_spaces(start=0, limit=500, expand='description.plain,homepage')
-        if ret['size'] == 0:
+        ret = self.__confluence.get_all_spaces(
+            start=0, limit=500, expand="description.plain,homepage"
+        )
+        if ret["size"] == 0:
             print("No spaces found in confluence. Please check credentials")
         for space in ret["results"]:
             if self.__space is None or space["key"] == self.__space:
@@ -177,7 +196,7 @@ class Converter:
             with open(path, "r", encoding="utf-8") as f:
                 data = f.read()
 
-            soup_raw = bs4.BeautifulSoup(data, 'html.parser')
+            soup_raw = bs4.BeautifulSoup(data, "html.parser")
             soup = self.__convert_atlassian_html(soup_raw)
 
             md = MarkdownConverter().convert_soup(soup)
@@ -206,12 +225,43 @@ if __name__ == "__main__":
         default=os.environ["CONFLUENCE_PASSWORD"],
         help="The access token to Confluence",
     )
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        default="confluence_export",
+        help="The directory to output the files to",
+    )
+    parser.add_argument(
+        "--space", type=str, required=False, default=None, help="Spaces to export"
+    )
+    parser.add_argument(
+        "--skip-attachments",
+        action="store_true",
+        dest="no_attach",
+        required=False,
+        default=False,
+        help="Skip fetching attachments",
+    )
+    parser.add_argument(
+        "--no-fetch",
+        action="store_true",
+        dest="no_fetch",
+        required=False,
+        default=False,
+        help="This option only runs the markdown conversion",
+    )
     args = parser.parse_args()
-    
+
     if not args.no_fetch:
-        dumper = Exporter(url=args.url, username=args.username, token=args.token, out_dir=args.out_dir,
-                          space=args.space, no_attach=args.no_attach)
+        dumper = Exporter(
+            url=args.url,
+            username=args.username,
+            token=args.token,
+            out_dir=args.out_dir,
+            space=args.space,
+            no_attach=args.no_attach,
+        )
         dumper.dump()
-    
+
     converter = Converter(out_dir=args.out_dir)
     converter.convert()
